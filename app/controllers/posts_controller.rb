6c73e5ea614d2b31106326b1cf11b5d90a5d02e3
naba0124 <68417762+naba0124@users.noreply.github.com>
2020-07-19 11:44:44 +0900

    postsコントローラー作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,4 @@
 class PostsController < ApplicationController
-
-  def index 
-    @posts = Post.all
+  def index
   end
-
-  def new
-  end
-
-  def create
-    Post.create(content: params[:content])
-  end
-end
+ end
